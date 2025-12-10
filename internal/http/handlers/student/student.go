118cef3d315a1d66fab7f5fa65e8b06f002acf27
Abhijit <abhijitkumarr1859@gmail.com>
2025-12-10 12:43:50 +0530

    validation done

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -3,11 +3,14 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 
 	"github.com/abhijit1859/REST_API_GOLANG/internal/types"
+	"github.com/abhijit1859/REST_API_GOLANG/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
@@ -16,10 +19,33 @@ func New() http.HandlerFunc {
 		var student types.Student
 
 		err:=json.NewDecoder(r.Body).Decode(&student)
-		if errors.Is(err,io.EOF{
-			
-		})
-		slog.Info("Creating a student")
-		w.Write([]byte("hello ji"))
+		
+		if errors.Is(err,io.EOF){
+			//returning json reponse
+			response.WriteJson(w,http.StatusBadRequest,response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+
+		if err!=nil{
+			response.WriteJson(w,http.StatusBadRequest,response.GeneralError(err))
+			return
+
+		}
+
+		//validating request
+		//dont trust the client
+		//jo chaiye tha whi mil rha na ?
+		//for validating it will use go-playground/validator
+
+		if err:=validator.New().Struct(student);err!=nil{
+			validateErrs:=err.(validator.ValidationErrors)
+			response.WriteJson(w,http.StatusBadRequest,response.ValidationError(validateErrs))
+			return
+		}
+
+
+
+		slog.Info("creating a student")
+		response.WriteJson(w,http.StatusCreated,map[string]string{"success":"ok"})
 	}
 }
--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -1,8 +1,59 @@
 package response
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Response struct{
+	Status string `json:"status"`
+	Error string	`json:"error"`
+}
+
+const (
+	StatusOK="OK"
+	StatusError="Error"
+)
+
 
 func WriteJson(w http.ResponseWriter,status int,data interface {}) error{
-	w.Header().set("Content-Type","application/json")
-	w.w
+	w.Header().Set("Content-Type","application/json")
+
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(data)
+	
+}
+
+func GeneralError(err error) Response{
+	return Response{
+		Status:StatusError,
+		Error:err.Error(),
+	}
+}
+
+
+
+func ValidationError(errs validator.ValidationErrors) Response{
+	var errMsgs []string
+
+	for _,err:=range errs{
+		switch err.ActualTag(){
+		case "required":
+			errMsgs=append(errMsgs, fmt.Sprintf("field %s isrequired field",err.Field()))
+
+		default:
+			errMsgs=append(errMsgs, fmt.Sprintf("field %s is invalid",err.Field()))		
+		}
+	}
+
+	return Response{
+		Status: StatusError,
+		Error: strings.Join(errMsgs,","),
+
+	}
 }
